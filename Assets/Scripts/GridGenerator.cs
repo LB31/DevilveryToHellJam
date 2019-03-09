3605f482dbf73009d120d78128d90091cd069cbd
LB31 <leonidb93@gmail.com>
2019-03-09 18:10:19 +0100

    let player move on grind

--- Assets/Scripts/GridGenerator.cs
+++ Assets/Scripts/GridGenerator.cs
@@ -7,6 +7,8 @@ public class GridGenerator : MonoBehaviour
 
     public GameObject grid;
 
+    public GameObject CubeToSpawn;
+
     [SerializeField]
     private MultiDimensionalInt[] gridBuilder;
     // Start is called before the first frame update
@@ -24,7 +26,8 @@ public class GridGenerator : MonoBehaviour
         for (int i = 0; i < gridBuilder.Length; i++) {
 
             for (int k = gridBuilder[i].startPosition; k < gridBuilder[i].amountCubes + gridBuilder[i].startPosition; k++) {
-                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                GameObject cube = Instantiate(CubeToSpawn);
                 cube.transform.position = new Vector3(k, 0f, i);
                 cube.tag = "Cube";
                 cube.transform.parent = grid.transform;
