748cf69ca335cc058298a48513cab3221c6fbf4e
LB31 <leonidb93@gmail.com>
2019-03-09 16:36:56 +0100

    make grid generator usable in editor

---
+++ Assets/Scripts/GridBuilderEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(GridGenerator))]
+public class GridBuilderEditor : Editor
+{
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        GridGenerator myScript = (GridGenerator)target;
+        if (GUILayout.Button("Build Object")) {
+            myScript.BuildGrid();
+        }
+    }
+}
+
